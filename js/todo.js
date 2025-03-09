9890a0f41fbf76af25d1b5e39d435063492b38f4
NovaCat <sparkstorms2@gmail.com>
2025-03-09 16:32:55 +0900

    Feat: delete method

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,21 @@ const toDoForm = document.getElementById("todo-form")
 const toDoText = document.getElementById("todo-text")
 const todoList = document.getElementById("todo-list")
 
+const deleteToDo = (event) => {
+    event.target.parentNode.remove()
+}
+
 const paintToDo = (newToDo) => {
     const newToDoItem = document.createElement("li")
     const spanText = document.createElement("span")
+    const deleteButton = document.createElement("button")
+    deleteButton.className = "todo-delete-button"
+    deleteButton.innerText = "❌"
+    deleteButton.addEventListener("click", deleteToDo)
+
     newToDoItem.appendChild(spanText)
+    newToDoItem.appendChild(deleteButton)
+
     spanText.innerText = newToDo
     todoList.appendChild(newToDoItem)
 }
